8668da775afa79bc3e04793af1311f570b81e6ab
wsargeant <69194027+wsargeant@users.noreply.github.com>
2020-12-23 08:08:48 +0000

    day_1 different methods

--- day_1.1.py
+++ day_1.1.py
@@ -17,31 +17,23 @@ def sum_2020(filename):
     """Returns two numbers from number list in filename which sum to 2020 and their product
     """
     number_list = read_integers(filename)
-    target = 2020
+    for x in number_list:
+        for y in number_list:
+            if x + y == 2020:
+                return x*y
 
-    for num1 in number_list:
-        num2 = target - num1
-        if num2 in number_list and num2 != num1:
-            break
-    return num1, num2, num1*num2
 
 def sum_2020_by_3(filename):
     """Returns three numbers from number list in filename which sum to 2020 and their product
     """
     number_list = read_integers(filename)
-    #reversed_number_list = number_list.copy()
-    #reversed_number_list.reverse()
-    target = 2020
 
-    for i in range(len(number_list)):
-        num1 = number_list[i]
-        for j in range(i,len(number_list)):
-            num2 = number_list[j]
-            num3 = target - num1 - num2
-            if num3 >= 0 and num3 in number_list:
-                return num1, num2, num3, num1*num2*num3
-    return False
- 
+    for x in number_list:
+        for y in number_list:
+            for z in number_list:
+                if x + y + z == 2020:
+                    return x*y*z
+
 
 print(sum_2020("day_1.txt"))
 
--- day_1.2.py
+++ day_1.2.py
@@ -17,31 +17,16 @@ def sum_2020(filename):
     """Returns two numbers from number list in filename which sum to 2020 and their product
     """
     number_list = read_integers(filename)
-    target = 2020
+    matches = [(x,y) for x in number_list for y in number_list if x+y == 2020]
+    return matches[0][0] * matches[0][1]
 
-    for num1 in number_list:
-        num2 = target - num1
-        if num2 in number_list and num2 != num1:
-            break
-    return num1, num2, num1*num2
 
 def sum_2020_by_3(filename):
     """Returns three numbers from number list in filename which sum to 2020 and their product
     """
     number_list = read_integers(filename)
-    #reversed_number_list = number_list.copy()
-    #reversed_number_list.reverse()
-    target = 2020
-
-    for i in range(len(number_list)):
-        num1 = number_list[i]
-        for j in range(i,len(number_list)):
-            num2 = number_list[j]
-            num3 = target - num1 - num2
-            if num3 >= 0 and num3 in number_list:
-                return num1, num2, num3, num1*num2*num3
-    return False
- 
+    matches = [(x,y,z) for x in number_list for y in number_list for z in number_list if x+y+z == 2020]   
+    return matches[0][0] * matches[0][1] * matches [0][2] 
 
 print(sum_2020("day_1.txt"))
 
